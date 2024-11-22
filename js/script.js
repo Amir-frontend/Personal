3df982b2df1cacaf81e90a4f3d329b744311af9f
Amir <hbbbbhh557@gmail.com>
2024-11-23 01:35:13 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+{
+// responceve web rastart
+let menu = document.getElementById("menu")
+menu.style.maxHeight = "0px";
+
+function menulest(){
+    if(menu.style.maxHeight == "0px")
+    {
+        menu.style.maxHeight = "300px";
+    }
+    else{
+        menu.style.maxHeight = "0px";
+    }
+}
+
+
+}
+
+
+
+
